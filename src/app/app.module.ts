735a6fe402f85be0322d84eb996eba04f2adb845
Marco <pichulol96@gmail.com>
2022-04-05 12:16:11 -0500

    empeanzo a trabajar en equipo con ramas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { UsuariosComponent } from './componentes/usuarios/usuarios.component';
+import { DetalleUsuarioComponent } from './componentes/detalle-usuario/detalle-usuario.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// http 
+import { HttpClientModule } from '@angular/common/http';
+import { RutaErrorComponent } from './componentes/ruta-error/ruta-error.component';
+import { HeaderComponent } from './componentes/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UsuariosComponent,
+    DetalleUsuarioComponent,
+    RutaErrorComponent,
+    HeaderComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
